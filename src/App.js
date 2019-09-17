3ecb24edd1d0b2c8c6907acfeb858c46013e676b
pipeza379 <kittapon.j@ku.th>
2019-09-17 14:40:40 +0700

    first setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+const App = () => (
+    <div>
+        <h1>Hello IWGU</h1>
+    </div>
+)
+
+export default hot(module)(App)
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: {
+            'react-dom': '@hot-loader/react-dom'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ],
+                        plugins: ['react-hot-loader/babel'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                exclude: /(node_modules)/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ],
+            },
+            {
+                test: /\.scss$/,
+                exclude: /(node_modules)/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg)$/,
+                exclude: /(node_modules)/,
+                use: [
+                    { loader: 'file-loader' },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+    devServer: {
+        hot: true,
+    },
+}
