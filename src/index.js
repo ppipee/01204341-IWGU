d45551f47b55d9deb37c25a3066e88aa9b972192
pipeza379 <kittapon.j@ku.th>
2019-09-17 21:07:26 +0700

    config route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware } from 'redux'
 // import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
-
 import reducer from './reducers'
 import './asset/css/index.css'
-import App from './App'
+import Routes from './Routes'
 
 // create redux store 
 const store = createStore(reducer, applyMiddleware(logger))
@@ -16,7 +16,9 @@ const store = createStore(reducer, applyMiddleware(logger))
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root')
 )
---
+++ src/pages/Home.js
@@ -0,0 +1,4 @@
+import React from 'react'
+const Home = () => <h1>Hello IWGU</h1>
+
+export default Home
