df5482249420553b8434207113303a31334a683b
pponjrc <pponjrc@pponjrcs-MacBook-Pro.local>
2019-09-19 11:55:57 +0700

    Navbar Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './../assets/scss/navbar.scss'
+import Menu from '../assets/icon/featured-menu.svg'
+import Airplane from '../assets/icon/airplane.svg'
 const NavBar = () => {
     return (
         <div className='nav-bar'>
-            NavBar
+            
+            <button className='button-mytrip'>
+            <div className='airplane-icon'>
+                <img className='airplane' src={Airplane}/>
+            </div>
+                <a className='string-mytrip'>My trips</a> 
+            </button>
+            
+            <img className='menu' src={Menu}/>
+            
         </div>
     )
 }
