27923d591b2fb239324d1bfb522a3bd788a520e5
thtiiz <napon-na123@hotmail.com>
2019-09-20 15:13:48 +0700

    feat(discover): card format and add swipe card

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,52 +1,68 @@
+import React, { Component } from 'react';
 import Slider from 'react-slick'
-import React from 'react'
 import './../assets/scss/explore.scss'
 import paper_airplane from '../assets/icon/paper-airplane.svg'
-import bangkok from '../assets/img/bangkok.jpg'
 
-class Explore extends React.Component {
+export default class Explore extends React.Component {
   render() {
-    var settings = {
+    let settings = {
       dots: true,
-      // infinite: true,
-      // speed: 500,
-      // slidesToShow: 1,
-      // slidesToScroll: 1
+      infinite: false,
+      arrows: false,
+      speed: 500,
+      slidesToShow: 1,
+      swipeToSlide: true,
+      centerMode: true,
+      centerPadding: '30px',
+      afterChange: function(index) {
+        console.log(
+          `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+        );
+      } 
     };
     return (
       <div className='explore'>
-        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link rel='stylesheet' type='text/css' charset='UTF-8' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
+        <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css' />
           <div className='explore-title'>
             <img src={paper_airplane} />
             <p > Explore </p>
           </div>
-          <div className='explore-card'>
-            
+          <div>
+            <div className='slider'> 
               <Slider {...settings}>
-                <div> 
-                  <img src={bangkok} />
+                <div className='explore-card'> 
+                  <div className='image'></div>
+                  <div className='box-title'>
+                    <p className='title'> 1 Bangkok, Thailand </p>
+                    <p className='subtitle'> The Beautiful temple near Chao Phraya River </p>
+                  </div>
                 </div>
-                <div>
-                  <h3>2</h3>
+                <div className='explore-card'> 
+                  <div className='image'></div>
+                  <div className='box-title'>
+                    <p className='title'> 2 Bangkok, Thailand </p>
+                    <p className='subtitle'> The Beautiful temple near Chao Phraya River </p>
+                  </div>
                 </div>
-                <div>
-                  <h3>3</h3>
+                <div className='explore-card'> 
+                  <div className='image'></div>
+                  <div className='box-title'>
+                    <p className='title'> 3 Bangkok, Thailand </p>
+                    <p className='subtitle'> The Beautiful temple near Chao Phraya River </p>
+                  </div>
                 </div>
-                <div>
-                  <h3>4</h3>
-                </div>
-                <div>
-                  <h3>5</h3>
-                </div>
-                <div>
-                  <h3>6</h3>
+                <div className='explore-card'> 
+                  <div className='image'></div>
+                  <div className='box-title'>
+                    <p className='title'> 4 Bangkok, Thailand </p>
+                    <p className='subtitle'> The Beautiful temple near Chao Phraya River </p>
+                  </div>
                 </div>
               </Slider>
-            
+            </div>  
           </div>
-        </div>    
+      </div>    
     )
   }
 }
-export default Explore
