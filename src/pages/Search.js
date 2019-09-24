aa625c56a8a96d745fee5f90e7b79fc0e2e91fdf
pipeza379 <kittapon.j@ku.th>
2019-09-24 23:52:30 +0700

    create Search page

---
+++ src/pages/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/Navbar'
+import SearchFilter from '../components/SearchFilter'
+
+const Search = () => {
+    return (
+        <div className="search-page">
+            <NavBar />
+            <SearchFilter />
+            Search-Page
+        </div>
+    )
+}
+
+export default Search
