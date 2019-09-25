6b694b652197789c720d0ae059ded3dacb2d9709
pipeza379 <kittapon.j@ku.th>
2019-09-25 15:38:35 +0700

    add result component

---
+++ src/components/SearchResult.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class SearchResult extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="search-result">
+            </div>
+        )
+    }
+}
+export default SearchResult
