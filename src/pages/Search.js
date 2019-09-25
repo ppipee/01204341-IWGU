7f0e81a9454653915fbf85ca81d0e72b542ddecb
pipeza379 <kittapon.j@ku.th>
2019-09-25 15:40:38 +0700

    add Filter modal

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import NavBar from '../components/Navbar'
 import SearchFilter from '../components/SearchFilter'
+import SearchResult from '../components/SearchResult'
+import Background from '../assets/img/fixed-background.svg'
+import '../assets/scss/searchpage.scss'
 
 const Search = () => {
     return (
         <div className="search-page">
             <NavBar />
-            <SearchFilter />
-            Search-Page
+            <div className="search-background" style={{ backgroundImage: `url(${Background})` }}>
+                <SearchFilter />
+                <SearchResult/>
+            </div>
         </div>
     )
 }
