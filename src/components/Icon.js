7a7be43a5ac54c3d6fe9b704514cc4873fd9b039
pipeza379 <kittapon.j@ku.th>
2019-09-26 13:18:13 +0700

    manage icon

---
+++ src/components/Icon.js
@@ -0,0 +1,17 @@
+import SearchIcon from '../assets/icon/search-icon.svg'
+import Filter from '../assets/icon/filter.svg'
+import FilterActive from '../assets/icon/filter-active.svg'
+import Clear from '../assets/icon/refresh.svg'
+import Star from '../assets/icon/full-star.svg'
+import BlankStar from '../assets/icon/empty-star.svg'
+import Close from '../assets/icon/close.svg'
+
+export {
+    SearchIcon,
+    Filter,
+    FilterActive,
+    Clear,
+    Star,
+    BlankStar,
+    Close
+}
