311a76386754d3682fcaa62aea45467bae8a2b09
pipeza379 <kittapon.j@ku.th>
2019-09-26 14:05:45 +0700

    adjust margin

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import SearchBar from './SearchBar'
 import '../assets/scss/searchfilter.scss'
-import Filter from '../assets/icon/filter.svg'
-import FilterActive from '../assets/icon/filter-active.svg'
+import { SearchIcon, Filter, FilterActive, Clear, Star, BlankStar, Close } from './Icon'
+import { Slider } from 'antd'
+
+// const tags = ['coffee shop', 'street food', 'landmark']
+const tags = []
+
 class SearchFilter extends Component {
     constructor(props) {
         super(props)
@@ -23,7 +27,7 @@ class SearchFilter extends Component {
     setWrapperRef = node => this.wrapperRef = node
 
     handleClickOutside = event => {
-        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+        if (this.wrapperRef && !this.wrapperRef.contains(event.target) && !this.state.filter) {
             this.setState({ filter: false })
         }
     }
@@ -31,20 +35,69 @@ class SearchFilter extends Component {
     clickFilter = () => this.setState(prevState => ({ filter: !prevState.filter }))
 
     actionFilter() {
-        let source = this.state.filter ? FilterActive : Filter
-        return (<img src={source} onClick={this.clickFilter} />)
+        let [source, name] = this.state.filter ? [FilterActive, "-active"] : [Filter, ""]
+        return (<img className={`img-filter${name}`} src={source} onClick={this.clickFilter} />)
     }
 
     showFilter() {
         if (this.state.filter) {
             return (
-                <div className={"filter-box open"} ref={this.setWrapperRef}>
+                <div className="filter-box open" ref={this.setWrapperRef}>
+                    <div className="sort">
+                        <div className="head">
+                            <span>Sort by :</span>
+                            <span>
+                                <span>clear</span>
+                                <img src={Clear} />
+                            </span>
+                        </div>
+                        <div className="sortby">
+                            <div>nearby</div>
+                            <div>rating</div>
+                            <div>price</div>
+                        </div>
+                    </div>
+                    <div className="category">
+                        <div className="head">Category:</div>
+                        <div className="tags">{this.genTag()}
+                            <div className="search">
+                                <img src={SearchIcon} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="line" />
+                    <div className="hour">
+                        <div className="head">Opening hours</div>
+                        <div className="options">
+                            <Slider range defaultValue={[20, 50]} />
+                        </div>
+                    </div>
+                    <div className="rating">
+                        <div className="head">Rating:</div>
+                        <div className="favs">
+                            {this.genFav()}
+                        </div>
+                    </div>
                 </div>
             )
         }
-        return (<div className={"filter-box"} ref={this.setWrapperRef} />)
+        return (<div className="filter-box" ref={this.setWrapperRef} />)
     }
 
+    genFav = () => {
+        let fav = []
+        for (let i = 1; i <= 5; i++) {
+            fav = [...fav,
+            <div className="fav" key={`fav-${i}`}>
+                <span>{i}</span>
+                <img src={Star} />
+            </div>
+            ]
+        }
+        return fav
+    }
+
+    genTag = () => tags.map((tag, i) => <div className="tag" key={`tag-${i}`}><div>{tag}</div><img src={Close} /></div>)
 
     render() {
         return (
