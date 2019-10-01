1fa83748fc57100a4c2fb1db5b430a6ab20df474
pponjrc <pponjrc@pponjrcs-MacBook-Pro.local>
2019-10-01 13:40:27 +0700

    sidebar_first

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,34 +3,64 @@ import './../assets/scss/navbar.scss'
 import Menu from '../assets/icon/featured-menu.svg'
 import Airplane from '../assets/icon/airplane.svg'
 class NavBar extends Component {
-    openNav() {
-        document.getElementById("mySidenav").style.width = "298px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    constructor(props){
+        super(props)
+        this.state = {
+            side_nav:false,
+        }
+    }
+    openNav = () => {
+        document.getElementById("mySidenav").style.width = "298px"        
+        document.getElementById("backDrop").style.display = "block"
+        this.setState({side_nav:true})
       }
-      /*window.onload = function(){
-        var divToHide = document.getElementById('mySidenav');
-        document.onclick = function(e){
-          if(e.target.id !== 'mySidenav'){
-            //element clicked wasn't the div; hide the div
-            mySidenav.style.display = 'none';
-          }
-        };
-      };*/
+      componentWillMount(){
+          document.addEventListener('mousedown',this.handleClick,false)
+      }
+      componentWillUnMount(){
+        document.removeEventListener('mousedown',this.handleClick,false)
+    }
+    handleClick = (e) =>{
+        if(this.node.contains(e.target)){
+            return;
+        }
+        this.handleClickOutside();
+    }
+    handleClickOutside(){
+        document.getElementById("mySidenav").style.width = "0px" 
+        document.getElementById("backDrop").style.display = "none"        
+        this.setState({side_nav:false})
+    }
+    genSideBar(){
+        if(this.state.side_nav){
+            return(
+                <>
+                    <div id="backDrop" className="containerSideNav" ref={node => this.node = node}  onClick={()=>this.handleClickOutside()}/>           
+                    <div id="mySidenav" className="sidenav" ref={node => this.node = node}  />
+                </>
+            )
+        }
+        return(                
+            <>            
+                <div id="backDrop" className="containerSideNav" ref={node => this.node = node} onClick={()=>this.handleClickOutside()}/>           
+                <div id="mySidenav" className="sidenav" ref={node => this.node = node}   />
+            </>
+        )
+    }
     render() {
         return (
             <div>
-                <div id="mySidenav" className="sidenav">
+                {this.genSideBar()}
+                <div className='nav-bar'>
+                    <button className='button-mytrip'>
+                        <img src={Airplane}/>
+                        My trips
+                    </button>
+                <span onClick={this.openNav}>
+                <img className='menu' src={Menu}/></span>
                 </div>
-            <div className='nav-bar'>
-            <button className='button-mytrip'>
-                <img src={Airplane}/>
-                My trips
-            </button>
-            <span onClick={this.openNav}>
-            <img className='menu' src={Menu}/></span>
-            </div>
 
-        </div>
+            </div>
     )
 }
 }
