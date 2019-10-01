a89d7ef11924a15131a84a64dafde8006c43363f
pponjrc <pponjrc@pponjrcs-MacBook-Pro.local>
2019-10-01 09:57:56 +0700

    show sidebar but cannot hide

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './../assets/scss/navbar.scss'
 import Menu from '../assets/icon/featured-menu.svg'
 import Airplane from '../assets/icon/airplane.svg'
-const NavBar = () => {
-    return (
-        <div className='nav-bar'>          
+class NavBar extends Component {
+    openNav() {
+        document.getElementById("mySidenav").style.width = "298px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+      }
+      /*window.onload = function(){
+        var divToHide = document.getElementById('mySidenav');
+        document.onclick = function(e){
+          if(e.target.id !== 'mySidenav'){
+            //element clicked wasn't the div; hide the div
+            mySidenav.style.display = 'none';
+          }
+        };
+      };*/
+    render() {
+        return (
+            <div>
+                <div id="mySidenav" className="sidenav">
+                </div>
+            <div className='nav-bar'>
             <button className='button-mytrip'>
                 <img src={Airplane}/>
                 My trips
-            </button>           
-            <img className='menu' src={Menu}/>            
+            </button>
+            <span onClick={this.openNav}>
+            <img className='menu' src={Menu}/></span>
+            </div>
+
         </div>
     )
 }
+}
+
 export default NavBar
