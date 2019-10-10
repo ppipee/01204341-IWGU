f2431f1039f29ed2dcf125e9504e86c126c1d374
ppipee <kittapon.j@ku.th>
2019-10-11 01:36:37 +0700

    component: create index

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Explore from './Explore'
+import NavBar from './NavBar'
+import NearBy from './NearBy'
+import SearchBar from './SearchBar'
+import SearchFilter from './SearchFilter'
+import SearchResult from './SearchResult'
+import CountDown from './CountDown'
+import PlannerBoard from './PlannersBoard'
+
+export {
+    CountDown,
+    Explore,
+    NavBar,
+    NearBy,
+    PlannerBoard,
+    SearchBar,
+    SearchFilter,
+    SearchResult,
+}
