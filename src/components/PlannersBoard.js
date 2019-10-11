ac54fd85f2a292ca724ac7cebc629195a08b854c
ppipee <kittapon.j@ku.th>
2019-10-12 01:31:09 +0700

    feat(planner): add space between planner and add-btn

--- src/components/PlannersBoard.js
+++ src/components/PlannersBoard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import AddTrip from './AddTrip'
+import CreateTrip from './CreateTrip'
 import { BlackAirplane, Calendar, CurveArrow, RightArrow } from './Icon'
 import '../assets/scss/plannersboard.scss'
 import { Planners } from './Demo'
@@ -57,10 +57,11 @@ class PlannersBoard extends Component {
             <div className='planners-board'>
                 <div className='title'>
                     <img src={BlackAirplane} alt='black-airplane-icon' />
-                    <span className='planners'>Manage your trip</span>
+                    <span>Manage your trip</span>
                 </div>
-                {this.plannersBoard()}
-                {/* <AddTrip /> */}
+                <div className='planners'>{this.plannersBoard()}</div>
+                <div className='blank' />
+                <CreateTrip />
             </div>
         )
     }
