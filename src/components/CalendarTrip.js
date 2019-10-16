53e9a9b1f661dba4aa78a1f913ce97ae92f91805
ppipee <kittapon.j@ku.th>
2019-10-17 02:03:21 +0700

    feat(mytrip): done day count & btn

--- src/components/CalendarTrip.js
+++ src/components/CalendarTrip.js
@@ -4,11 +4,24 @@ import '../assets/scss/calendartrip.scss'
 class CalendarTrip extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            day: 0,
+        }
     }
 
     render() {
-        return <div className='calendar'>calendar</div>
+        const day = this.state.day ? 's' : ''
+        return (
+            <div className='calendar-trip'>
+                <div className='state'>
+                    <div className={`day ${this.state.day}`}>
+                        <span className='days-count'>{this.state.day}</span>
+                        <span>{`Day${day}`}</span>
+                    </div>
+                    <div className='start-trip-btn'>Start your trip</div>
+                </div>
+            </div>
+        )
     }
 }
 export default CalendarTrip
