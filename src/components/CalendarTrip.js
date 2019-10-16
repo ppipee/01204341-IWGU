9676595e4964f32f66c1c177031f6805c3192f4c
ppipee <kittapon.j@ku.th>
2019-10-17 01:21:07 +0700

    setup(mytrip): adjust struct

---
+++ src/components/CalendarTrip.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../assets/scss/calendartrip.scss'
+
+class CalendarTrip extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return <div className='calendar'>calendar</div>
+    }
+}
+export default CalendarTrip
