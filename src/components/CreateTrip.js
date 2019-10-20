c0573ae5a1307b4c9f2957873c5df613d0ec0e52
ppipee <kittapon.j@ku.th>
2019-10-20 18:05:02 +0700

    set(mytrip): set redux of create my trip

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Plus } from './Icon'
+import { NewTripAction } from '../action'
 import HeadCreateTrip from './HeadCreateTrip'
 import CalendarTrip from './CalendarTrip'
 import '../assets/scss/createtrip.scss'
@@ -9,6 +11,44 @@ class CreateTrip extends Component {
         super()
         this.state = {
             plan_maker: false,
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.nextProps.getAuth) {
+            this.createTrip()
+        }
+    }
+
+    calDay = (start, end) =>
+        start ? (end - start) / (1000 * 60 * 60 * 24) + 1 : 0
+
+    createTrip = () => {
+        if (this.props.getAuth) {
+            const userid = this.props.getUserID
+            const { name, date } = this.props
+            const day = this.calDay(date.start, date.end)
+            let days = []
+            for (let i = 0; i < day; i++) {
+                const current_date = date.start
+                days = [
+                    ...days,
+                    {
+                        day: i,
+                        date: new Date(
+                            current_date.setDate(current_date.getDate() + i)
+                        ),
+                        places: [],
+                        note: '',
+                    },
+                ]
+            }
+            console.log(days)
+            const trip = {
+                userID: userid,
+                name,
+                days,
+            }
         }
     }
 
@@ -35,6 +75,7 @@ class CreateTrip extends Component {
     }
 
     render() {
+        this.createTrip()
         return (
             <>
                 <img
@@ -48,4 +89,15 @@ class CreateTrip extends Component {
         )
     }
 }
-export default CreateTrip
+const mapStateToProps = state => {
+    return {
+        getName: state.newtrip.name,
+        getDate: state.newtrip.date,
+        getCreateAuth: state.newtrip.auth,
+        getUserID: state.userauth.userid,
+    }
+}
+export default connect(
+    mapStateToProps,
+    null
+)(CreateTrip)
