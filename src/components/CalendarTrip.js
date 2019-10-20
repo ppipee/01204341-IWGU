d3c048492db1c1affdedcaebe3bd9920d18e9d95
ppipee <kittapon.j@ku.th>
2019-10-20 18:06:22 +0700

    feat(mytrip): calendar done and setup redux

--- src/components/CalendarTrip.js
+++ src/components/CalendarTrip.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-datepicker'
 import { DateRangePicker } from 'react-dates'
+import { connect } from 'react-redux'
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
+import { NewTripAction } from '../action'
+import { LeftArrowColor, RightArrowColor } from './Icon'
 import '../assets/scss/calendartrip.scss'
 
+const Action = NewTripAction
 class CalendarTrip extends Component {
     constructor() {
         super()
         this.state = {
-            day: 0,
             start_date: null,
             end_date: null,
             focused_input: 'startDate',
+        }
+    }
+
+    setTrip = () => {
+        const name = this.props.getName
+        const { start_date: start, end_date: end } = this.state
+        if (name !== '' && (start !== null && end !== null)) {
+            const date = {
+                start: start.toDate(),
+                end: end.toDate(),
+            }
+            this.props.setDate(date)
+            this.props.setAuth(true)
         }
     }
 
@@ -21,43 +37,83 @@ class CalendarTrip extends Component {
         this.setState({ focused_input })
     }
 
+    calDay = () =>
+        this.state.start_date
+            ? (this.state.end_date - this.state.start_date) /
+                  (1000 * 60 * 60 * 24) +
+              1
+            : 0
+
     render() {
-        const day = this.state.day ? 's' : ''
-        console.log(this.state.focused_input)
+        const day = this.calDay() > 1 ? 's' : ' '
         return (
             <div className='calendar-trip'>
                 <div className='calendar'>
+                    <div className='calendar-arrow'>
+                        <div className='calendar-arrow-left'>
+                            <span className='calendar-line' />
+                            <img src={LeftArrowColor} alt='left-arrow-color' />
+                        </div>
+                        <div className='calendar-arrow-right'>
+                            <img
+                                src={RightArrowColor}
+                                alt='right-arrow-color'
+                            />
+                            <span className='calendar-line' />
+                        </div>
+                    </div>
                     <DateRangePicker
                         startDate={this.state.start_date}
                         endDate={this.state.end_date}
                         startDateId='startDate'
                         endDateId='endDate'
-                        onDatesChange={({ startDate, endDate }) =>
+                        onDatesChange={({ startDate, endDate }) => {
+                            if (endDate === null) endDate = startDate
+                            if (startDate === null) startDate = endDate
                             this.setState({
                                 start_date: startDate,
                                 end_date: endDate,
                             })
-                        }
+                        }}
                         onFocusChange={focused_input =>
                             this.onFocusChange(focused_input)
                         }
                         focusedInput={this.state.focused_input}
                         displayFormat='D MMM'
+                        monthFormat='MMM Y'
                         numberOfMonths={1}
-                        small
                         minimumNights={0}
                         daySize={32}
+                        enableOutsideDays
+                        startDatePlaceholderText='start'
+                        endDatePlaceholderText='end'
                     />
                 </div>
-                {/* <div className='state'>
+                <div className='state'>
                     <div className={`day ${this.state.day}`}>
-                        <span className='days-count'>{this.state.day}</span>
-                        <span>{`Day${day}`}</span>
+                        <div className='days-count'>{this.calDay()}</div>
+                        <div>{`Day${day}`}</div>
                     </div>
-                    <div className='start-trip-btn'>Start your trip</div>
-                </div> */}
+                    <div className='start-trip-btn' onClick={this.setTrip}>
+                        Start your trip
+                    </div>
+                </div>
             </div>
         )
     }
 }
-export default CalendarTrip
+const mapDispatchToProps = dispatch => {
+    return {
+        setDate: date => dispatch({ type: Action.SETDATE, setDate: date }),
+        setAuth: auth => dispatch({ type: Action.SETAUTH, setDate: auth }),
+    }
+}
+const mapStateToProps = state => {
+    return {
+        getName: state.newtrip.name,
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CalendarTrip)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import searching from './SearchReducer'
 import filters from './FilterReducer'
+import newtrip from './NewTripReducer'
+import userauth from './UserAuth'
 
 export default combineReducers({
     searching,
     filters,
+    newtrip,
+    userauth,
 })
