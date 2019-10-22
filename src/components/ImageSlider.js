9833f855d15a41d7d4d04c81e92b0b370e917ce8
ppipee <kittapon.j@ku.th>
2019-10-23 02:27:03 +0700

    feat(detail-page): add animation when click img

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '../assets/scss/imageslider.scss'
 import { Images } from './Demo'
+import Background from '../assets/img/img-slider-bg.svg'
 
 class ImageSlider extends Component {
     constructor(props) {
@@ -10,8 +11,9 @@ class ImageSlider extends Component {
         }
     }
 
-    handleImage = e => {
+    handleImage = async e => {
         const index = e.target.getAttribute('index')
+        this.sliceAnimation(index)
         this.setState({ image_show: index })
     }
 
@@ -24,15 +26,17 @@ class ImageSlider extends Component {
                     src={check ? Images[this.state.image_show] : ''}
                     key={`img-foucus-${i + 1}`}
                     alt='img-focus'
-                    style={
-                        check ? { transform: translateX(`-${20 * i} px`) } : {}
-                    }
                 />
             )
         })
 
     genImages = () => (
-        <div className='images-container'>
+        <div
+            className='images-container'
+            ref={node => {
+                this.node = node
+            }}
+        >
             {Images.map((img, index) => (
                 <div
                     className={`image-contain ${
@@ -51,9 +55,24 @@ class ImageSlider extends Component {
         </div>
     )
 
+    sliceAnimation = index => {
+        const position = 91 * index - this.node.scrollLeft
+        for (let i = 0; i < Math.abs(position); i++) {
+            setTimeout(() => {
+                if (position >= 0) this.node.scrollBy(1, 0)
+                else this.node.scrollBy(-1, 0)
+            }, 1)
+        }
+    }
+
     render() {
         return (
             <div className='image-slider'>
+                <img
+                    className='slider-bg'
+                    src={Background}
+                    alt='img-slider-bg'
+                />
                 <div className='image-focus'>{this.focusImage()}</div>
                 {this.genImages()}
             </div>
