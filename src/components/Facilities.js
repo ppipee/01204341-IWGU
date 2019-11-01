e2ef37ccd2eb4cc595a5992383a4ad74467b881b
earnchanya <earnnneooo@gmail.com>
2019-11-01 20:06:52 +0700

    feat(detail): fix icon

--- src/components/Facilities.js
+++ src/components/Facilities.js
@@ -1,79 +1,69 @@
 import React, { Component } from 'react'
 import '../assets/scss/facilities.scss'
 import {
-    CoinInactive,
-    CoinActive,
-    CreditcardInactive,
-    CreditcardActive,
-    GroupInactive,
-    GroupActive,
-    HandicapInactive,
-    HandicapActive,
-    KidInactive,
-    KidActive,
-    ParkingInactive,
-    ParkingActive,
-    PetInactive,
-    PetActive,
-    PlugInactive,
-    PlugActive,
-    SmokingInactive,
-    SmokingActive,
-    WifiInactive,
-    WifiActive,
+    Service,
+    Creditcard,
+    Group,
+    Handicap,
+    Kid,
+    Parking,
+    Pet,
+    Plug,
+    Smoking,
+    Wifi,
     ArrowDown,
     ArrowUp,
 } from './Icon'
 
 const FacilityList = [
     {
-        icon_inactive: WifiInactive,
-        icon_active: WifiActive,
+        icon_inactive: <Wifi fill='#B0B0B0' />,
+        icon_active: <Wifi fill='#FCBBA4' />,
         title: 'WIFI available',
     },
     {
-        icon_inactive: CreditcardInactive,
-        icon_active: CreditcardActive,
+        icon_inactive: <Creditcard fill='#B0B0B0' />,
+        icon_active: <Creditcard fill='#FCBBA4' />,
         title: 'Credit/Debit card',
     },
     {
-        icon_inactive: ParkingInactive,
-        icon_active: ParkingActive,
+        icon_inactive: <Parking fill='#B0B0B0' />,
+        icon_active: <Parking fill='#FCBBA4' />,
         title: 'Car parks',
     },
     {
-        icon_inactive: PlugInactive,
-        icon_active: PlugActive,
+        icon_inactive: <Plug fill='#B0B0B0' />,
+        icon_active: <Plug fill='#FCBBA4' />,
         title: 'Free charge',
     },
     {
-        icon_inactive: GroupInactive,
-        icon_active: GroupActive,
+        icon_inactive: <Group fill='#B0B0B0' />,
+        icon_active: <Group fill='#FCBBA4' />,
         title: 'Group',
     },
     {
-        icon_inactive: HandicapInactive,
-        icon_active: HandicapActive,
+        icon_inactive: <Handicap fill='#B0B0B0' />,
+        icon_active: <Handicap fill='#FCBBA4' />,
         title: 'Handicapped accessible',
     },
     {
-        icon_inactive: KidInactive,
-        icon_active: KidActive,
+        icon_inactive: <Kid fill='#B0B0B0' />,
+        icon_active: <Kid fill='#FCBBA4' />,
         title: 'Kids',
     },
     {
-        icon_inactive: CoinInactive,
-        icon_active: CoinActive,
+        icon_inactive: <Service fill='#B0B0B0' />,
+        icon_active: <Service fill='#FCBBA4' />,
         title: 'Service charge',
     },
     {
-        icon_inactive: PetInactive,
-        icon_active: PetActive,
+        icon_inactive: <Pet fill='#B0B0B0' />,
+        icon_active: <Pet fill='#FCBBA4' />,
         title: 'Pet allowed',
     },
     {
-        icon_inactive: SmokingInactive,
-        icon_active: SmokingActive,
+        icon_inactive: <Smoking fill='#B0B0B0' />,
+        icon_active: <Smoking fill='#FCBBA4' />,
         title: 'Smoking area',
     },
 ]
@@ -97,12 +87,6 @@ export default class Facilities extends Component {
         }
     }
 
-    // componentDidUpdate (prevProps, prevState) {
-    //     if (prevState.all_icon === this.state.all_icon) {
-    //         {this.checkFacilities()}
-    //     }
-    // }
-
     componentDidMount() {
         const { service } = this.props
         const all_icon = []
@@ -117,20 +101,6 @@ export default class Facilities extends Component {
         this.setState({ all_icon })
     }
 
-    // checkFacilities = () => {
-    //     const service = this.props.service
-    //     const all_icon = []
-    //     if (service.facilities === null) {
-    //         for (let i = 0; i < FacilityList.length; i++) {
-    //             all_icon.push(false)
-    //         }
-    //     }
-    //     if (service.payment !== null) {
-    //         all_icon[1] = true
-    //     }
-    //     this.setState({all_icon})
-    // }
-
     facilityDetail = () => {
         const facility_show = []
         const facility_hidden = []
@@ -138,14 +108,9 @@ export default class Facilities extends Component {
             if (i <= 5) {
                 facility_show.push(
                     <div className='facility-box' key={facility.title}>
-                        <img
-                            src={
-                                this.state.all_icon[i]
-                                    ? facility.icon_active
-                                    : facility.icon_inactive
-                            }
-                            alt='facility-icon-active'
-                        />
+                        {this.state.all_icon[i]
+                            ? facility.icon_active
+                            : facility.icon_inactive}
                         <p
                             className={`facility-title ${
                                 this.state.all_icon[i] ? '' : 'inactive'
@@ -158,14 +123,9 @@ export default class Facilities extends Component {
             } else if (i > 5 && this.state.show_more) {
                 facility_hidden.push(
                     <div className='facility-box' key={facility.title}>
-                        <img
-                            src={
-                                this.state.all_icon[i]
-                                    ? facility.icon_active
-                                    : facility.icon_inactive
-                            }
-                            alt='facility-icon-active'
-                        />
+                        {this.state.all_icon[i]
+                            ? facility.icon_active
+                            : facility.icon_inactive}
                         <p
                             className={`facility-title ${
                                 this.state.all_icon[i] ? '' : 'inactive'
