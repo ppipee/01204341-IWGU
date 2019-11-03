8b2d6a063ca00ea0b0bed8766636d45b53dc3501
ppipee <kittapon.j@ku.th>
2019-11-04 03:23:06 +0700

    core: add local storage (userauth)

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import logger from 'redux-logger'
+import storage from 'redux-persist/lib/storage'
+import rootReducer from '../reducers'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['newtrip'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+    const store = createStore(persistedReducer, applyMiddleware(logger))
+    const persistor = persistStore(store)
+    return { store, persistor }
+}
