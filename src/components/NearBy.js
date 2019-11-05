40c89a333f81034fe02401d7f4b58917a72c4c0d
Chutchawin Thaengthong <chutchawin.work@gmail.com>
2019-11-05 15:09:06 +0700

    feat(Contact): comment checkplace function

--- src/components/NearBy.js
+++ src/components/NearBy.js
@@ -45,51 +45,52 @@ class NearBy extends Component {
     }
 
     // https://www.geodatasource.com/developers/javascript =============
-    checkplace(lat2, lon2) {
-        const { map } = this.props
-        const lat1 = map.latitude
-        const lon1 = map.longitude
-        let check = false
-        let dist
-        if (lat1 === lat2 && lon1 === lon2) {
-            check = true
-        } else {
-            const radlat1 = (Math.PI * lat1) / 180
-            const radlat2 = (Math.PI * lat2) / 180
-            const theta = lon1 - lon2
-            const radtheta = (Math.PI * theta) / 180
-            dist =
-                Math.sin(radlat1) * Math.sin(radlat2) +
-                Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
-            if (dist > 1) {
-                dist = 1
-            }
-            dist = Math.acos(dist)
-            dist = (dist * 180) / Math.PI
-            dist = dist * 60 * 1.1515 * 1.609344
-            if (dist < 10) {
-                check = true
-            }
-        }
-        return check
-    }
+    // checkplace(lat2, lon2) {
+    //     const { map } = this.props
+    //     const lat1 = map.latitude
+    //     const lon1 = map.longitude
+    //     let check = false
+    //     let dist
+    //     if (lat1 === lat2 && lon1 === lon2) {
+    //         check = true
+    //     } else {
+    //         const radlat1 = (Math.PI * lat1) / 180
+    //         const radlat2 = (Math.PI * lat2) / 180
+    //         const theta = lon1 - lon2
+    //         const radtheta = (Math.PI * theta) / 180
+    //         dist =
+    //             Math.sin(radlat1) * Math.sin(radlat2) +
+    //             Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+    //         if (dist > 1) {
+    //             dist = 1
+    //         }
+    //         dist = Math.acos(dist)
+    //         dist = (dist * 180) / Math.PI
+    //         dist = dist * 60 * 1.1515 * 1.609344
+    //         if (dist < 10) {
+    //             check = true
+    //         }
+    //     }
+    //     return check
+    // }
     // ===============================================================
 
     genPlace(places, id) {
         const box = []
         places.forEach((place, i) => {
-            if (this.checkplace(place.map.latitude, place.map.longitude)) {
-                box.push(
-                    <div key={`${place.name}-${i + 1}`}>
-                        <img
-                            className='picture'
-                            alt={place.name}
-                            src={place.thumbnail}
-                        />
-                        <div className='name'>{place.name}</div>
-                    </div>
-                )
-            }
+            console.log(place.map.latitude)
+            // if (this.checkplace(place.map.latitude, place.map.longitude)) {
+            box.push(
+                <div key={`${place.name}-${i + 1}`}>
+                    <img
+                        className='picture'
+                        alt={place.name}
+                        src={place.thumbnail}
+                    />
+                    <div className='name'>{place.name}</div>
+                </div>
+            )
+            // }
         })
         return (
             <div id={id} className='tab-content'>
